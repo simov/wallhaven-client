6c8d584165ff735eaf3f8ecfbb255cf658af720b
simov <simeonvelichkov@gmail.com>
2018-08-03 15:46:30 +0300

    Update example

--- examples/download-random.js
+++ examples/download-random.js
@@ -7,14 +7,18 @@ var random = (min, max) =>
 ;(async () => {
   try {
     // https://alpha.wallhaven.cc/search?purity=100&atleast=1920x1080&sorting=random
-    var {wallpapers} = await wh.search({
+    var params = {
       purity: '100',
       atleast: '1920x1080',
       sorting: 'random',
-    })
-    // download one of the 24 returned wallpapers
+    }
+    // get pages count for this filter
+    var {pages} = await wh.search(params)
+    // pick random page from this filter
+    var {wallpapers} = await wh.search({...params, page: random(1, pages)})
+    // pick random wallpaper from the returned page
     await wh.download({
-      wallpapers: wallpapers[random(0, 23)],
+      wallpapers: wallpapers[random(0, wallpapers.length - 1)],
       size: 'full',
     })
     console.log('DONE!')
